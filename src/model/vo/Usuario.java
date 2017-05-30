15790f2cbd995ea35bcdf9fa735585c69c33a427
Guilherme <guilherme.h12@gmail.com>
2017-05-29 23:59:24 -0300

    vcxgdsf

--- src/model/vo/Usuario.java
+++ src/model/vo/Usuario.java
@@ -1,7 +1,17 @@
 package model.vo;
 
+import java.util.ArrayList;
+
 public class Usuario {
 	public Long id;
 	public String nome;
 	public String cpf;
+	public String rua;
+	public int numero;
+	public String bairro;
+	public String cidade;
+	public String uf;
+	public String pais;
+	
+	public ArrayList<>
 }
